43919586e88ed0606176893fe8d598e6a54c9d79
miShowmik <miskatshowmik@gmail.com>
2021-01-05 22:30:51 +0600

    "new"

--- src/loopsInJAVA/simpleQuizGame.java
+++ src/loopsInJAVA/simpleQuizGame.java
@@ -1,5 +1,26 @@
 package loopsInJAVA;
+import java.util.Scanner;
 
 public class simpleQuizGame {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number1 = (int)(Math.random() *10);
+        int number2 = (int)(Math.random() *10);
+
+        System.out.println("what is "+ number1+ " + "+ number2+"?");
+
+        int answer = input.nextInt();
+
+        while(number1+number2 != answer){
+            System.out.println("wrong answer! try again. What is "+ number1+ " + "+ number2+"?");
+            answer = input.nextInt();
+        }
+
+        System.out.println("you are right! ");
+
+
+
+    }
 
 }
