56d3f6fa45b6e079c646d29fbb5872efaa224a1e
Showmik <53708135+miShowmik@users.noreply.github.com>
2021-01-10 19:21:39 +0600

    "hp"

---
+++ src/loopsInJAVA/decimalToHexa.java
@@ -0,0 +1,27 @@
+package loopsInJAVA;
+import java.util.Scanner;
+
+public class decimalToHexa {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("enter the decimal value: ");
+        int decimal = input.nextInt();
+
+        String hex = " ";
+        while(decimal!=0){
+            int hexValue = decimal % 16;
+
+            char hexDigit = (hexValue <= 9 && hexValue>=0) ? (char)(hexValue+'0'): (char)(hexValue - 10 + 'A');
+
+            hex = hexDigit + hex;
+            decimal = decimal /16;
+
+        }
+
+        System.out.println("the hex number is : "+ hex);
+
+
+    }
+}
